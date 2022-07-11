cad991ccf83358f4a208312e323b75293921dd79
HarmonyMorse <harmonymorse@icloud.com>
2022-07-11 11:19:37 -0700

    Added text field (for movie name) and buttons (to save or cancel) to the detail view controller.

--- Movie List/Comments.swift
+++ Movie List/Comments.swift
@@ -11,6 +11,7 @@ import Foundation
 /*
  TO DO NOW:
 
+
  
  FINISHED:
  A Movie model object that has properties for:
@@ -22,7 +23,10 @@ import Foundation
  The table view should use a custom UITableViewCell subclass with a "Seen"/"Not Seen" button.
  + Added label for movie name
  
- A bar button item that segues to a new view controller. This new view controller allows the user to add new movies. 
+ A bar button item that segues to a new view controller. This new view controller allows the user to add new movies.
+ 
+ The view controller should have a text field to put in the movie's name, and a button to save the movie.
+ + Cancel button
  
  */
 
@@ -30,8 +34,6 @@ import Foundation
 /*
  Your finished project must include all of the following requirements:
  Tapping the button toggles whether the user has seen the movie or not. (This should change the Movie's Bool property that represents if it has been seen or not)
-
- The view controller should have a text field to put in the movie's name, and a button to save the movie.
  
  
  Stretch Problems:
