b278c8c778503ce39310527d6afddfe1be7f27d7
HarmonyMorse <harmonymorse@icloud.com>
2022-07-11 10:53:35 -0700

    Created a Movie model object that has properties for the movie's name as a String and whether it has been seen or not as a Bool (default value of false). Also sorted files into organized groups

---
+++ Movie List/Comments.swift
@@ -0,0 +1,43 @@
+//
+//  Comments.swift
+//  Movie List
+//
+//  Created by Harm on 7/11/22.
+//  Copyright © 2022 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+/*
+ TO DO NOW:
+ 
+ 
+ FINISHED:
+ A Movie model object that has properties for:
+ The movie's name as a String
+ Whether it has been seen or not as a Bool. You can set a default value for this property to false if you want.
+ 
+ 
+ */
+
+
+
+
+
+
+
+/*
+ Your finished project must include all of the following requirements:
+ 
+ A view controller that shows a list of movies that the user has created. This should be a subclass of UIViewController that has a UITableView in it.
+ The table view should use a custom UITableViewCell subclass with a "Seen"/"Not Seen" button. Tapping the button toggles whether the user has seen the movie or not. (This should change the Movie's Bool property that represents if it has been seen or not)
+ A bar button item that segues to a new view controller. This new view controller allows the user to add new movies. The view controller should have a text field to put in the movie's name, and a button to save the movie.
+ 
+ 
+ Stretch Problems:
+
+ Users can swipe to delete movies in the table view.
+ Introduce a way to edit the name of a movie.
+ The list of movies is preserved between subsequent runs of your application
+
+ */
---
+++ Movie List/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Harm on 7/11/22.
+//  Copyright © 2022 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    var movieName: String
+    var seen: Bool = false
+}
